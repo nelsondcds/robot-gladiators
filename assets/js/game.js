a2c7a5a9da9ba086590a0729c2dac6eb094e8b28
nelsondcds <pipoca012008@yahoo.com>
2021-06-13 16:20:56 -0400

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -66,7 +66,13 @@ var fight = function(enemyName) {
 window.alert("Welcome to Robot Gladiators!");
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        window.alert("Round " + (i + 1) + "!");
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+    } else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
 }
